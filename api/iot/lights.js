451146059395bc36d8dc949ece3f2e9e5d513fd3
Phil Lutas <phil@lutas.co.uk>
2017-09-12 20:13:34 +0100

    Attempt to fix multiple scans of the same device causing a stall when loading lights on rpi

--- api/iot/lights.js
+++ api/iot/lights.js
@@ -16,30 +16,23 @@ let wemo = new Wemo();
 let index = lights.length;
 // discover Belkin lights
 console.log('Scanning for Wemo devices');
+
+let belkinLights = {};
 wemo.discover(function(err, deviceInfo) {
 
-    function getIndexFor(friendlyName) {
-        for (let i = 0; i < lights.length; ++i) {
-            if (lights[i].friendlyName == friendlyName) {
-                return i;
-            }
-        }
-
-        return null;
-    }
-
-    // if is light
+    // if the device is a compatible light or lightswitch
     if (deviceInfo.deviceType == Wemo.DEVICE_TYPE.Switch) {
 
         let url = 'http://' + deviceInfo.host + ':' + deviceInfo.port;
 
         // if this light already exists then replace it, otherwise create a new index
-        let existingIndex = getIndexFor(deviceInfo.friendlyName);
+        let existingIndex = belkinLights[deviceInfo.friendlyName];
         console.log('Setting Belkin light:', deviceInfo.friendlyName, 'to', existingIndex || index);
         if (existingIndex) {
             lights[existingIndex] = new BelkinLight(existingIndex, deviceInfo.friendlyName, url);
         } else {            
             lights.push(new BelkinLight(index, deviceInfo.friendlyName, url));
+            belkinLights[deviceInfo.friendlyName] = index;
             ++index;
         }
     }
